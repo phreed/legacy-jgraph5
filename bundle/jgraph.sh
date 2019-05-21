276e82f81e0a79b3da6660629f5f32d8f4b27bfb
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2019-05-21 16:02:32 -0500

    reorganized elements of bundle

--- bundle/jgraph.sh
+++ bundle/jgraph.sh
@@ -1,17 +1,19 @@
 # mvn gpg:sign-and-deploy-file \
 # 	-Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
 # 	-DrepositoryId=ossrh \
-# 	-DpomFile=jgraph-5.12.3.2.pom \
-# 	-Dfile=jgraph-5.12.3.2.jar
+# 	-DpomFile=${PROJ}.pom \
+# 	-Dfile=${PROJ}.jar
 
-gpg -ab jgraph-5.12.3.2.pom
-gpg -ab jgraph-5.12.3.2.jar
-gpg -ab jgraph-sources-5.12.3.2.jar
-gpg -ab jgraph-javadoc-5.12.3.2.jar
-jar -cvf jgraph-bundle.jar \
-	 jgraph-5.12.3.2.pom jgraph-5.12.3.2.pom.asc \
-	 jgraph-5.12.3.2.jar jgraph-5.12.3.2.jar.asc \
-	 jgraph-sources-5.12.3.2.jar jgraph-sources-5.12.3.2.jar.asc \
-	 jgraph-javadoc-5.12.3.2.jar jgraph-javadoc-5.12.3.2.jar.asc
+PROJ="jgraph-5.12.3.2"
+
+gpg -ab ${PROJ}.pom
+gpg -ab ${PROJ}.jar
+gpg -ab ${PROJ}-javadoc.jar
+gpg -ab ${PROJ}-sources.jar
+jar -cvf ${PROJ}-bundle.jar \
+	 ${PROJ}.pom ${PROJ}.pom.asc \
+	 ${PROJ}.jar ${PROJ}.jar.asc \
+	 ${PROJ}-javadoc.jar ${PROJ}-javadoc.jar.asc \
+	 ${PROJ}-sources.jar ${PROJ}-sources.jar.asc
 
 
